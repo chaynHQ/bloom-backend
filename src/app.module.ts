1c454b4435dc8bc9f60dd51a4b44d983b0c8a277
Seyi Bakare <oluwaseyi.bakare@gmail.com>
2021-10-27 23:03:27 +0100

    Dockerized bloom backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { isProduction } from './constants';
 import { LoggerModule } from './logger/logger.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: `.env.${process.env.NODE_ENV}`,
-      isGlobal: true,
-      ignoreEnvFile: isProduction,
-      cache: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        synchronize: false,
+        migrationsRun: true,
+        entities: [],
+      }),
     }),
     LoggerModule,
-    TypeOrmModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [],
