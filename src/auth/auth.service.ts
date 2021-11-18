d8b577232b5df6ed889cf9369f94e634d209f817
Seyi Bakare <oluwaseyi.bakare@gmail.com>
2021-11-18 10:41:32 +0000

    Code refactor

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { FIREBASE } from 'src/firebase/firebase-factory';
+import { FirebaseServices } from 'src/firebase/firebase.types';
+import { UserAuthDto } from './dto/user-auth.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(@Inject(FIREBASE) private firebase: FirebaseServices) {}
+
+  public async loginFirebaseUser({ email, password }: UserAuthDto) {
+    return await this.firebase.auth.signInWithEmailAndPassword(email, password);
+  }
+}
