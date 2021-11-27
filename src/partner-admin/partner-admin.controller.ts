64c5c16b7667608d33a0bf4f1635de665f8fe82c
Seyi Bakare <oluwaseyi.bakare@gmail.com>
2021-11-27 20:22:08 +0000

    Implementation updates

--- src/partner-admin/partner-admin.controller.ts
+++ src/partner-admin/partner-admin.controller.ts
@@ -1,10 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiProduces, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PartnerAdminEntity } from 'src/entities/partner-admin.entity';
 import { CreatePartnerAdminDto } from './dtos/create-partner-admin.dto';
 import { PartnerAdminService } from './partner-admin.service';
 
 @ApiTags('Partner Admin')
+@ApiConsumes('application/json')
+@ApiProduces('application/json')
+@ApiResponse({ status: 201, description: 'The record has been successfully created.' })
+@ApiResponse({ status: 400, description: 'Incorrect payload sent.' })
+@ApiResponse({ status: 401, description: 'Unauthorized.' })
+@ApiResponse({ status: 403, description: 'Forbidden.' })
+@ApiResponse({ status: 500, description: 'Internal Server Error.' })
 @Controller('/v1/partner-admin')
 export class PartnerAdminController {
   constructor(private partnerAdminService: PartnerAdminService) {}
