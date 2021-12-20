ea57f267762712a3d862690af75e1b8d1bfcfc65
Seyi Bakare <oluwaseyi.bakare@gmail.com>
2021-12-20 01:22:10 +0000

    Remove unnecessary imports

--- src/partner-access/partner-access.controller.ts
+++ src/partner-access/partner-access.controller.ts
@@ -7,8 +7,6 @@ import { PartnerAdminAuthGuard } from '../partner-admin/partner-admin-auth.guard
 import { PartnerAccessEntity } from '../entities/partner-access.entity';
 import { ValidatePartnerAccessCodeDto } from './dtos/validate-partner-access.dto';
 import { PartnerAccessCodeStatusEnum } from '../utils/constants';
-import { SimplybookBodyDto } from './dtos/zapier-body.dto';
-import { ZapierAuthGuard } from './zapier-auth.guard';
 import { SuperAdminAuthGuard } from '../partner-admin/super-admin-auth.guard';
 import { ControllerDecorator } from '../utils/controller.decorator';
 
