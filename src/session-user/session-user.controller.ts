9f88d1b75d868a7a1f1640322eebe1698a0f7fc7
Seyi Bakare <oluwaseyi.bakare@gmail.com>
2022-01-16 19:05:43 +0000

    Create Session User Endpoint

--- src/session-user/session-user.controller.ts
+++ src/session-user/session-user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { FirebaseAuthGuard } from '../firebase/firebase-auth.guard';
+import { ControllerDecorator } from '../utils/controller.decorator';
+import { CreateSessionUserDto } from './dtos/create-session-user.dto';
+import { SessionUserService } from './session-user.service';
+import { Request } from 'express';
 
+@ApiTags('Session User')
+@ControllerDecorator()
 @Controller('session-user')
-export class SessionUserController {}
+export class SessionUserController {
+  constructor(private readonly sessionUserService: SessionUserService) {}
+
+  @Post()
+  @ApiBearerAuth()
+  @UseGuards(FirebaseAuthGuard)
+  async createSessionUserRecord(
+    @Body() createSessionUserDto: CreateSessionUserDto,
+    @Req() res: Request,
+  ) {
+    return '';
+  }
+}
--- src/session-user/session-user.service.ts
+++ src/session-user/session-user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CourseUserService } from 'src/course-user/course-user.service';
+import { SessionUserRepository } from './session-user.repository';
 
 @Injectable()
-export class SessionUserService {}
+export class SessionUserService {
+  constructor(
+    @InjectRepository(SessionUserRepository) private sessionUserRepository: SessionUserRepository,
+    private readonly courseUserService: CourseUserService,
+  ) {}
+
+  public async createSessionUser() {}
+}
