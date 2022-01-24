a9151deb3c291502804e22dc6ff38e5812b5c69f
Seyi Bakare <oluwaseyi.bakare@gmail.com>
2022-01-24 15:24:46 +0000

    Import dependencies

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseController } from './course.controller';
+import { CourseRepository } from './course.repository';
 import { CourseService } from './course.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CourseRepository])],
   controllers: [CourseController],
-  providers: [CourseService]
+  providers: [CourseService],
 })
 export class CourseModule {}
