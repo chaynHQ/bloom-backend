1eb621d723c77c5b4b20032269574218d80a3d16
Seyi Bakare <Seyi-Baks@users.noreply.github.com>
2022-03-09 08:26:21 +0000

    Staging Release: Extend getUsers (#118)
    * Removed valide access code function (#108)
    
    * Crisp refactor (#107)
    
    Refactor crisp
    
    * fix courseData ref (#112)
    
    * Link SimplyBook data with our database (#110)
    
    * Simplybook Entity & Migration file
    
    * WIP
    
    * Ability to save Simplybook data into db
    
    * PartnerAccess One to Many TherapySession
    
    * WIP
    
    * Update Therapy Session refactor
    
    * Minor updates
    
    * Code refactor, new migration script, improved logic
    
    * Format therapy session object
    
    * renaming
    
    * refactor updateTherapySession (#114)
    
    Co-authored-by: annarhughes <annaraehughes@live.co.uk>
    
    * Extend get user (#116)
    
    * Simplybook Entity & Migration file
    
    * WIP
    
    * Ability to save Simplybook data into db
    
    * PartnerAccess One to Many TherapySession
    
    * WIP
    
    * Update Therapy Session refactor
    
    * Format Partner Accesses
    
    * Fixed naming
    
    * Minor updates
    
    * Code refactor, new migration script, improved logic
    
    * Format therapy session object
    
    Co-authored-by: Anna Hughes <annaraehughes@live.co.uk>

---
+++ src/webhooks/therapy-session.interface.ts
@@ -0,0 +1,17 @@
+import { SIMPLYBOOK_ACTION_ENUM } from 'src/utils/constants';
+
+export interface ITherapySession {
+  id?: string;
+  action?: SIMPLYBOOK_ACTION_ENUM;
+  client_email?: string;
+  client_timezone?: string;
+  service_name?: string;
+  service_provider_name?: string;
+  service_provider_email?: string;
+  start_date_time?: Date;
+  end_date_time?: Date;
+  cancelledAt?: Date;
+  rescheduledFrom?: Date;
+  completedAt?: Date;
+  partnerAccessId?: string;
+}
