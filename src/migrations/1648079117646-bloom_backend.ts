060535f835c3919088cc7caadfb409f8f5a4104e
Seyi Bakare <oluwaseyi.bakare@gmail.com>
2022-03-24 16:29:46 +0000

    Update to migration file

--- src/migrations/1648079117646-bloom_backend.ts
+++ src/migrations/1648079117646-bloom_backend.ts
@@ -4,18 +4,16 @@ export class bloomBackend1648079117646 implements MigrationInterface {
   name = 'bloomBackend1648079117646';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "languageDefault" TO "isActive"`);
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "languageDefault"`);
     await queryRunner.query(`ALTER TABLE "partner" DROP COLUMN "logo"`);
     await queryRunner.query(`ALTER TABLE "partner" DROP COLUMN "primaryColour"`);
-    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isActive"`);
     await queryRunner.query(`ALTER TABLE "user" ADD "isActive" boolean NOT NULL DEFAULT true`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isActive"`);
-    await queryRunner.query(`ALTER TABLE "user" ADD "isActive" character varying NOT NULL`);
     await queryRunner.query(`ALTER TABLE "partner" ADD "primaryColour" character varying`);
     await queryRunner.query(`ALTER TABLE "partner" ADD "logo" character varying`);
-    // await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "isActive" TO "languageDefault"`);
+    await queryRunner.query(`ALTER TABLE "user" ADD "languageDefault" character varying NOT NULL`);
   }
 }
