61772371b2c0cb4ae30426530287598614d892d4
Seyi Bakare <oluwaseyi.bakare@gmail.com>
2022-03-24 16:13:18 +0000

    Update to migration file

--- src/migrations/1648079117646-bloom_backend.ts
+++ src/migrations/1648079117646-bloom_backend.ts
@@ -1,22 +1,21 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class bloomBackend1648079117646 implements MigrationInterface {
-    name = 'bloomBackend1648079117646'
+  name = 'bloomBackend1648079117646';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "languageDefault" TO "isActive"`);
-        await queryRunner.query(`ALTER TABLE "partner" DROP COLUMN "logo"`);
-        await queryRunner.query(`ALTER TABLE "partner" DROP COLUMN "primaryColour"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isActive"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "isActive" boolean NOT NULL DEFAULT true`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "languageDefault" TO "isActive"`);
+    await queryRunner.query(`ALTER TABLE "partner" DROP COLUMN "logo"`);
+    await queryRunner.query(`ALTER TABLE "partner" DROP COLUMN "primaryColour"`);
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isActive"`);
+    await queryRunner.query(`ALTER TABLE "user" ADD "isActive" boolean NOT NULL DEFAULT true`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isActive"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "isActive" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "partner" ADD "primaryColour" character varying`);
-        await queryRunner.query(`ALTER TABLE "partner" ADD "logo" character varying`);
-        await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "isActive" TO "languageDefault"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isActive"`);
+    await queryRunner.query(`ALTER TABLE "user" ADD "isActive" character varying NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "partner" ADD "primaryColour" character varying`);
+    await queryRunner.query(`ALTER TABLE "partner" ADD "logo" character varying`);
+    // await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "isActive" TO "languageDefault"`);
+  }
 }
