0ddf18a230ed250cf6193df10caef13c48507ddf
Seyi Bakare <oluwaseyi.bakare@gmail.com>
2022-04-04 22:00:30 +0100

    generated new migration file

--- seed-local-db.sh
+++ seed-local-db.sh
@@ -8,7 +8,7 @@ TIMESTAMP=`date "+%Y%m%d-%H%M%S"`
 set -e
 
 #Backup Remote database
-curl `heroku pg:backups public-url --app <HEROKU_APP_NAME>` > bloom_$TIMESTAMP.dump
+curl `heroku pg:backups public-url --app bloom-backend-staging` > bloom_$TIMESTAMP.dump
 
 #Load backup into local database
 docker exec -i bloom-local-db pg_restore -U postgres -d bloom < bloom_$TIMESTAMP.dump
