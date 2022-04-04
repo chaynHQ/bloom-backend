08c68f3050260ec5158bfe1584349597fd9b4a78
Seyi Bakare <Seyi-Baks@users.noreply.github.com>
2022-04-04 22:13:37 +0100

    Remove partner (#147)
    * Update Entity
    
    * Delete Partner
    
    * generated new migration file

--- src/partner/partner.module.ts
+++ src/partner/partner.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PartnerAccessRepository } from '../partner-access/partner-access.repository';
+import { PartnerAdminRepository } from '../partner-admin/partner-admin.repository';
 import { UserRepository } from '../user/user.repository';
 import { PartnerController } from './partner.controller';
 import { PartnerRepository } from './partner.repository';
 import { PartnerService } from './partner.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PartnerRepository, UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      PartnerRepository,
+      UserRepository,
+      PartnerAccessRepository,
+      PartnerAdminRepository,
+    ]),
+  ],
   controllers: [PartnerController],
   providers: [PartnerService],
 })
