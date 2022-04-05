4677137472f19932b0fa52431e53c71cb3617e4a
annarhughes <annaraehughes@live.co.uk>
2022-04-05 14:25:20 +0100

    refactor + fixes

--- src/session-user/dtos/update-session-user.dto.ts
+++ src/session-user/dtos/update-session-user.dto.ts
@@ -1,15 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsDefined, IsNotEmpty } from 'class-validator';
+import { IsDefined, IsNotEmpty } from 'class-validator';
 
 export class UpdateSessionUserDto {
   @IsNotEmpty()
   @IsDefined()
   @ApiProperty({ type: Number })
   storyblokId: number;
-
-  @IsBoolean()
-  @IsDefined()
-  @IsNotEmpty()
-  @ApiProperty({ type: Boolean })
-  completed: boolean;
 }
