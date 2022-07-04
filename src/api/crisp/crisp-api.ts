118d49c1f6c07f1774ec16acebf49f17fa574b97
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-07-04 10:20:51 +0100

    Extract createCrispProfileData into seperate file and test

--- src/api/crisp/crisp-api.ts
+++ src/api/crisp/crisp-api.ts
@@ -10,9 +10,11 @@ import {
   NewPeopleProfileResponse,
   PeopleData,
 } from './crisp-api.interfaces';
-
-// https://docs.crisp.chat/references/rest-api/v1/#add-new-people-profile
-// Full details needed to add a new person
+import {
+  createCrispProfileData,
+  formatCourseKey,
+  formatSessionKey,
+} from './utils/createCrispProfileData';
 
 const baseUrl = `https://api.crisp.chat/v1/website/${crispWebsiteId}`;
 
@@ -20,58 +22,6 @@ const headers = {
   Authorization: `Basic ${crispToken}`,
   'X-Crisp-Tier': 'plugin',
   '-ContentType': 'application/json',
-};
-
-const getAcronym = (text: string) => {
-  return `${text
-    .split(/\s/)
-    .reduce((response, word) => (response += word.slice(0, 1)), '')
-    .toLowerCase()}`;
-};
-
-const formatCourseKey = (courseName: string) => {
-  return `course_${getAcronym(courseName)}_status`;
-};
-
-const formatSessionKey = (courseName: string, status: PROGRESS_STATUS) => {
-  return `course_${getAcronym(courseName)}_sessions_${status.toLowerCase()}`;
-};
-
-export const createCrispProfileData = (
-  user: IUser,
-  partnerAccesses: IPartnerAccessWithPartner[],
-  courses?: ICoursesWithSessions[],
-) => {
-  let profileData = {
-    partners: partnerAccesses.map((pa) => pa.partner.name).join('; '),
-    feature_live_chat: !!partnerAccesses.find((pa) => !!pa.featureLiveChat),
-    feature_therapy: !!partnerAccesses.find((pa) => !!pa.featureTherapy),
-    therapy_sessions_remaining: partnerAccesses
-      .map((pa) => pa.therapySessionsRemaining)
-      .reduce((a, b) => a + b),
-    therapy_sessions_redeemed: partnerAccesses
-      .map((pa) => pa.therapySessionsRedeemed)
-      .reduce((a, b) => a + b),
-  };
-
-  if (!!courses && courses.length > 0) {
-    const courseData = {};
-
-    courses.forEach((course) => {
-      const courseKey = formatCourseKey(course.name);
-      courseData[`${courseKey}`] = course.status;
-
-      const sessionsStartedKey = formatSessionKey(course.name, PROGRESS_STATUS.STARTED);
-      const sessionsStarted = course.sessions.filter((session) => !session.completed).join('; ');
-      courseData[`${sessionsStartedKey}`] = sessionsStarted;
-      const sessionsCompletedKey = formatSessionKey(course.name, PROGRESS_STATUS.COMPLETED);
-      const sessionsCompleted = course.sessions.filter((session) => !!session.completed).join('; ');
-      courseData[`${sessionsCompletedKey}`] = sessionsCompleted;
-    });
-    profileData = Object.assign({}, profileData, courseData);
-  }
-
-  return profileData;
 };
 
 export const updateCrispProfileAccesses = async (
