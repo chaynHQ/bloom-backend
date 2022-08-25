6ade11f8b08ac26f7d7cd300503e4defa6567d2c
swetha-chayn <swetha@chayn.co>
2022-08-25 18:45:41 +0100

    Update table to reflect general email campaigns
    240822-1831 / Add database table to track therapy feedback emails

--- src/webhooks/therapy-feedback/therapy-feedback.repository.ts
+++
@@ -1,5 +0,0 @@
-import { TherapyFeedbackEntity } from 'src/entities/therapy-feedback.entity';
-import { EntityRepository, Repository } from 'typeorm';
-
-@EntityRepository(TherapyFeedbackEntity)
-export class TherapyFeedbackRepository extends Repository<TherapyFeedbackEntity> {}
