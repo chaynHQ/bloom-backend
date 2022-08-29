580597e6cd879f4faf1fafa0967f3066f3aae17f
swetha-chayn <swetha@chayn.co>
2022-08-29 19:15:22 +0100

    Update timestamp to not be nullable
    Since an entry in this table means an email has been sent, this field does not need to be nullable.
    
    240822-1831 / Add database table to track therapy feedback emails

--- src/migrations/1661449620908-bloom_backend.ts
+++ src/migrations/1661449620908-bloom_backend.ts
@@ -5,7 +5,7 @@ export class bloomBackend1661449620908 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "email_campaign" ("createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "campaignType" character varying NOT NULL, "campaignInfo" character varying NOT NULL, "email" character varying NOT NULL, "emailSentDateTime" TIMESTAMP WITH TIME ZONE, CONSTRAINT "UQ_e510ab9687d29667a0c49c57a0e" UNIQUE ("campaignInfo"), CONSTRAINT "PK_140462ad4b82245fec89e5a3976" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "email_campaign" ("createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "id" uuid NOT NULL DEFAULT uuid_generate_v4(), "campaignType" character varying NOT NULL, "campaignInfo" character varying NOT NULL, "email" character varying NOT NULL, "emailSentDateTime" TIMESTAMP WITH TIME ZONE NOT NULL, CONSTRAINT "UQ_e510ab9687d29667a0c49c57a0e" UNIQUE ("campaignInfo"), CONSTRAINT "PK_140462ad4b82245fec89e5a3976" PRIMARY KEY ("id"))`,
     );
   }
 
