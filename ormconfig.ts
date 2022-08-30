a9a2e83bb56eb47a3123927036c23ec553d4a66c
swetha-chayn <swetha@chayn.co>
2022-08-30 13:16:19 +0100

    Update orm config to run db migrations on startup
    This will ensure that staging and production have similar db migrations behaviour.
    
    Previously, production db was set up to run migrations on startup and staging was not.
    
    240822-1831 / Add database table to track therapy feedback emails

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,6 +4,11 @@ import { databaseUrl, isProduction } from './src/utils/constants';
 
 const { host, port, user, password, database } = PostgressConnectionStringParser.parse(databaseUrl);
 
+/**
+ * Notes on connection options:
+ * synchronize: this setting will update the database automatically without running migrations. Turn on with caution.
+ * migrationsRun: this setting ensures migrations are run on the db at startup. Turn off with caution.
+ *  */
 const config: PostgresConnectionOptions = {
   type: 'postgres',
   host,
@@ -13,7 +18,7 @@ const config: PostgresConnectionOptions = {
   database,
   entities: ['dist/src/**/*.entity.js'],
   synchronize: false,
-  migrationsRun: isProduction,
+  migrationsRun: true,
   migrations: ['dist/src/migrations/*.js'],
   cli: {
     migrationsDir: 'src/migrations',
