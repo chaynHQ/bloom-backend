857eaad3c9da89f6b012e37bfa55217cc71a23c5
swetha-chayn <swetha@chayn.co>
2022-08-30 14:34:11 +0100

    Set up simplybook api support
    Add method to support pulling client booking information for a particular date.
    
    This endpoint will be used to collate a list of emails which need a therapy feedback email to be sent.
    
    300822-1432 / Update therapy endpoint to request simplybook data

---
+++ src/api/simplybook/simplybook-api.ts
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+import { simplybookCompanyName, simplybookCredentials } from 'src/utils/constants';
+
+type Booking = {
+  client: {
+    name: string;
+    email: string;
+  };
+  code: string;
+};
+
+type SimplybookBookingInfo = {
+  clientEmail: string;
+  bookingCode: string;
+  date: Date;
+};
+
+const DATE_FORMAT_LENGTH = 'YYYY-mm-dd'.length;
+
+const getAuthToken: () => Promise<string> = async () => {
+  const response = await axios({
+    method: 'post',
+    url: 'https://user-api-v2.simplybook.me/admin/auth',
+    data: JSON.parse(simplybookCredentials),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return response.data.token;
+};
+
+const getBookingsForDate: (date: Date) => Promise<Booking[]> = async (date: Date) => {
+  const token = await getAuthToken();
+
+  const simplybookFilterDateString = date.toISOString().substring(0, DATE_FORMAT_LENGTH);
+
+  const bookingsResponse = await axios({
+    method: 'get',
+    url: `https://user-api-v2.simplybook.me/admin/bookings?filter[date]=${simplybookFilterDateString}&filter[status]=confirmed`,
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Company-Login': simplybookCompanyName,
+      'X-Token': `${token}`,
+    },
+  });
+
+  return bookingsResponse.data.data;
+};
+
+export const getBookingInfoForDate: (date: Date) => Promise<SimplybookBookingInfo[]> = async (
+  date: Date,
+) => {
+  const bookings: Booking[] = await getBookingsForDate(date);
+
+  return bookings.map((booking) => ({
+    clientEmail: booking.client.email,
+    bookingCode: booking.code,
+    date: date,
+  }));
+};
