311bd29a6424142da66a493082b8b3f3f63d5f90
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-08-31 15:15:46 +0100

    Add mock for test mailchimp

--- src/api/mailchimp/mailchip-api.spec.ts
+++ src/api/mailchimp/mailchip-api.spec.ts
@@ -1,5 +1,13 @@
 import { healthCheck } from './mailchip-api';
 
+jest.mock('@mailchimp/mailchimp_transactional', () => () => ({
+  users: {
+    ping: async () => {
+      return 'PONG!';
+    },
+  },
+}));
+
 describe('Mailchimp api', () => {
   it('healthCheck', async () => {
     await expect(healthCheck()).resolves.toEqual('PONG!');
