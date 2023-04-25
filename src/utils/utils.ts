f20e99a7721c0898e061ee903b75e352cbd9558d
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-04-25 16:30:59 +0100

    Make generate random string stronger

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,17 @@
+import { webcrypto } from 'crypto';
+const crypto = webcrypto as unknown as Crypto;
+
 export const generateRandomString = (length: number) => {
-  return [...Array(length)].map(() => (~~(Math.random() * 36)).toString(36)).join('');
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  const randomValues = new Uint32Array(length);
+  const cryptoedValues = crypto.getRandomValues(randomValues);
+
+  return [...cryptoedValues]
+    .map((val) => {
+      return characters.charAt(val % charactersLength);
+    })
+    .join('');
 };
 
 export const getYesterdaysDate = () => new Date(new Date().setDate(new Date().getDate() - 1));
