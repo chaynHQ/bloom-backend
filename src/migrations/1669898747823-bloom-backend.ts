d0e8a6cfaf8be02e9d6819fc8d2facbf0bc62f32
Atreay Kukanur <66585295+ATREAY@users.noreply.github.com>
2023-10-09 03:30:39 +0000

    Moved README.md documentation to new /docs directory

--- src/migrations/1669898747823-bloom-backend.ts
+++ src/migrations/1669898747823-bloom-backend.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class bloomBackend1669898747823 implements MigrationInterface {
-    name = 'bloomBackend1669898747823'
+  name = 'bloomBackend1669898747823';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "signUpLanguage" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" ADD "signUpLanguage" character varying`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "signUpLanguage"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "signUpLanguage"`);
+  }
 }
