3a2a49d75167d9f9ce21f362da5f6db48475bb0e
annarhughes <annaraehughes@live.co.uk>
2024-06-04 20:01:03 +0100

    add missing user module imports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SlackMessageClient } from 'src/api/slack/slack-api';
+import { ZapierWebhookClient } from 'src/api/zapier/zapier-webhook-client';
 import { PartnerAccessEntity } from 'src/entities/partner-access.entity';
 import { PartnerAdminEntity } from 'src/entities/partner-admin.entity';
 import { PartnerEntity } from 'src/entities/partner.entity';
+import { SubscriptionUserEntity } from 'src/entities/subscription-user.entity';
+import { SubscriptionEntity } from 'src/entities/subscription.entity';
+import { TherapySessionEntity } from 'src/entities/therapy-session.entity';
 import { UserEntity } from 'src/entities/user.entity';
 import { SubscriptionUserService } from 'src/subscription-user/subscription-user.service';
+import { SubscriptionService } from 'src/subscription/subscription.service';
 import { TherapySessionService } from 'src/therapy-session/therapy-session.service';
 import { AuthService } from '../auth/auth.service';
 import { FirebaseModule } from '../firebase/firebase.module';
@@ -14,7 +20,15 @@ import { UserService } from './user.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserEntity, PartnerEntity, PartnerAccessEntity, PartnerAdminEntity]),
+    TypeOrmModule.forFeature([
+      UserEntity,
+      PartnerEntity,
+      SubscriptionEntity,
+      SubscriptionUserEntity,
+      PartnerAccessEntity,
+      PartnerAdminEntity,
+      TherapySessionEntity,
+    ]),
     FirebaseModule,
   ],
   controllers: [UserController],
@@ -22,8 +36,11 @@ import { UserService } from './user.service';
     UserService,
     AuthService,
     PartnerAccessService,
+    SubscriptionService,
     SubscriptionUserService,
     TherapySessionService,
+    ZapierWebhookClient,
+    SlackMessageClient,
   ],
 })
 export class UserModule {}
