3978d94bd1db7a86d75860eace04926f06959101
Anna Hughes <annaraehughes@live.co.uk>
2024-06-14 15:17:18 +0100

    chore: improve create user firebase error logging (#476)

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,4 +1,9 @@
-export const CREATE_USER_FIREBASE_ERROR = 'CREATE_USER_FIREBASE_ERROR';
-export const CREATE_USER_INVALID_EMAIL = 'CREATE_USER_INVALID_EMAIL';
-export const CREATE_USER_WEAK_PASSWORD = 'CREATE_USER_WEAK_PASSWORD';
-export const CREATE_USER_ALREADY_EXISTS = 'CREATE_USER_ALREADY_EXISTS';
+// Includes known/expected firebase errors from user inputs
+// These errors are returned to the user and display appropriate error messages
+// However these errors are ignored from rollbar and error logging
+export enum FIREBASE_ERRORS {
+  CREATE_USER_FIREBASE_ERROR = 'CREATE_USER_FIREBASE_ERROR',
+  CREATE_USER_INVALID_EMAIL = 'CREATE_USER_INVALID_EMAIL',
+  CREATE_USER_WEAK_PASSWORD = 'CREATE_USER_WEAK_PASSWORD',
+  CREATE_USER_ALREADY_EXISTS = 'CREATE_USER_ALREADY_EXISTS',
+}
