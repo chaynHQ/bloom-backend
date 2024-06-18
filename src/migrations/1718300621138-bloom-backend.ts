a954896c926db2bd7f8f645478553fdf2c7ebc1f
annarhughes <annaraehughes@live.co.uk>
2024-06-18 15:33:26 +0100

    fix migration

--- src/migrations/1718300621138-bloom-backend.ts
+++ src/migrations/1718300621138-bloom-backend.ts
@@ -1,16 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class BloomBackend1718300621138 implements MigrationInterface {
-    name = 'BloomBackend1718300621138'
+  name = 'BloomBackend1718300621138';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastActiveAt"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "lastActiveAt" TIMESTAMP WITH TIME ZONE`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" ADD "lastActiveAt" TIMESTAMP WITH TIME ZONE`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastActiveAt"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "lastActiveAt" date`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "lastActiveAt"`);
+  }
 }
