e273b4380ceede13693b0badcca206367148e1ce
Anna Hughes <annaraehughes@live.co.uk>
2024-08-21 19:35:29 +0100

    fix: rm bulk delete (#601)

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,7 +9,7 @@ import { Logger } from 'src/logger/logger';
 import { ServiceUserProfilesService } from 'src/service-user-profiles/service-user-profiles.service';
 import { SubscriptionUserService } from 'src/subscription-user/subscription-user.service';
 import { TherapySessionService } from 'src/therapy-session/therapy-session.service';
-import { isProduction, SIGNUP_TYPE } from 'src/utils/constants';
+import { SIGNUP_TYPE } from 'src/utils/constants';
 import { FIREBASE_ERRORS } from 'src/utils/errors';
 import { FIREBASE_EVENTS, USER_SERVICE_EVENTS } from 'src/utils/logs';
 import { ILike, IsNull, Not, Repository } from 'typeorm';
@@ -324,23 +324,6 @@ export class UserService {
     }
 
     this.logger.log(`deleteCypressTestUsers - successfully deleted ${deletedUsers.length} users`);
-    return deletedUsers;
-  }
-
-  public async bulkDeleteUsers(): Promise<UserEntity[]> {
-    if (isProduction) {
-      throw new Error('Bulk delete cannot be performed on production database');
-    }
-
-    let deletedUsers: UserEntity[];
-
-    try {
-      const users = await this.userRepository.find();
-
-      deletedUsers = await this.batchDeleteUsers(users);
-    } catch (error) {
-      this.logger.error(`deleteFilteredUsers - Unable to delete all users`, error);
-    }
     return deletedUsers;
   }
 
