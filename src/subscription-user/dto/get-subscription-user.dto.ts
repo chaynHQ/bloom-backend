8fdadef7630ce93f1b16f3cce78749f840a9d7f9
eleanorreem <eleanor.reem@hotmail.co.uk>
2024-09-10 15:44:17 +0200

    Update get-subscription-user.dto.ts

--- src/subscription-user/dto/get-subscription-user.dto.ts
+++ src/subscription-user/dto/get-subscription-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Expose, Type } from 'class-transformer';
+import { Expose } from 'class-transformer';
 import { IsDefined, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 @Expose()
