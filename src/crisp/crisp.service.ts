41704ea4d643201e1c83dcdc9467fcf2adc76fd2
annarhughes <annaraehughes@live.co.uk>
2024-10-14 16:59:22 +0100

    logger events

--- src/crisp/crisp.service.ts
+++ src/crisp/crisp.service.ts
@@ -1,57 +1,59 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import Crisp from 'crisp-api';
-import { UserEntity } from 'src/entities/user.entity';
 import { EventLoggerService } from 'src/event-logger/event-logger.service';
 import { crispPluginId, crispPluginKey } from 'src/utils/constants';
-import { Repository } from 'typeorm';
 import { EVENT_NAME } from './crisp.interface';
 import { CrispEventDto } from './dtos/crisp.dto';
 
 const CrispClient = new Crisp();
+const logger = new Logger('CrispLogger');
 
 @Injectable()
 export class CrispService implements OnModuleInit {
-  constructor(
-    private eventLoggerService: EventLoggerService,
+  constructor(private eventLoggerService: EventLoggerService) {
+    CrispClient.authenticateTier('plugin', crispPluginId, crispPluginKey);
+  }
 
-    @InjectRepository(UserEntity) private userRepository: Repository<UserEntity>,
-  ) {}
+  async handleCrispEvent(message: CrispEventDto, eventName: EVENT_NAME) {
+    const sessionMetaData = await CrispClient.website.getConversationMetas(
+      message.website_id,
+      message.session_id,
+    );
+    this.eventLoggerService.createEventLog({
+      email: sessionMetaData.email,
+      event: eventName,
+      date: new Date(),
+    });
+  }
 
   onModuleInit() {
-    console.log(`Crisp service initialised`);
+    logger.log(`Crisp service initiated`);
 
-    CrispClient.authenticateTier('plugin', crispPluginId, crispPluginKey);
+    try {
+      const handleCrispEvent = async (message, eventName) =>
+        await this.handleCrispEvent(message, eventName);
 
-    CrispClient.on('message:send', function (message: CrispEventDto) {
-      console.log('message sent data:', message);
-      this.eventLoggerService.createEventLog({
-        email: message.user.email,
-        event: EVENT_NAME.CHAT_MESSAGE_SENT,
-        date: Date.now(),
-      });
-    })
-      .then(function () {
-        console.error('Crisp service listening to sent messages');
+      CrispClient.on('message:send', async function (message: CrispEventDto) {
+        handleCrispEvent(message, EVENT_NAME.CHAT_MESSAGE_SENT);
       })
-      .catch(function (error) {
-        console.error('Crisp service failed listening to sent messages:', error);
-      });
+        .then(function () {
+          logger.log('Crisp service listening to sent messages');
+        })
+        .catch(function (error) {
+          logger.error('Crisp service failed listening to sent messages:', error);
+        });
 
-    CrispClient.on('message:receive', function (message: CrispEventDto) {
-      console.log('message received data:', message);
-
-      this.eventLoggerService.createEventLog({
-        email: message.user.email,
-        event: EVENT_NAME.CHAT_MESSAGE_RECEIVED,
-        date: Date.now(),
-      });
-    })
-      .then(function () {
-        console.error('Crisp service listening to received messages');
+      CrispClient.on('message:received', function (message: CrispEventDto) {
+        handleCrispEvent(message, EVENT_NAME.CHAT_MESSAGE_RECEIVED);
       })
-      .catch(function (error) {
-        console.error('Crisp service failed listening to sent messages:', error);
-      });
+        .then(function () {
+          logger.log('Crisp service listening to received messages');
+        })
+        .catch(function (error) {
+          logger.error('Crisp service failed listening to sent messages:', error);
+        });
+    } catch (error) {
+      logger.error('Crisp service failed to initiate:', error);
+    }
   }
 }
