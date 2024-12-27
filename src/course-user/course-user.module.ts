4432b442a73797be6e7fb51eabf37f3dc746fce6
Attila Cseh <attila.cseh@outlook.hu>
2024-12-27 12:42:14 +0000

    CoursesUserController.getCourseUserByUserId endpoint created

--- src/course-user/course-user.module.ts
+++ src/course-user/course-user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseUserEntity } from 'src/entities/course-user.entity';
 import { CourseUserService } from './course-user.service';
+import { CoursesUserController } from './courses-user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CourseUserEntity])],
+  controllers: [CoursesUserController],
   providers: [CourseUserService],
 })
 export class CourseUserModule {}
