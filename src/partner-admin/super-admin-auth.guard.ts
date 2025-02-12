99affb1b54c21d680adb03465192990f628b66fa
Anna Hughes <annaraehughes@live.co.uk>
2025-02-12 22:54:13 +0000

    feat: superadmin multifactor auth (#772)

--- src/partner-admin/super-admin-auth.guard.ts
+++ src/partner-admin/super-admin-auth.guard.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Request } from 'express';
+import { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
 import { UserEntity } from 'src/entities/user.entity';
 import { AUTH_GUARD_ERRORS, FIREBASE_ERRORS } from 'src/utils/errors';
 import { Repository } from 'typeorm';
@@ -33,10 +34,10 @@ export class SuperAdminAuthGuard implements CanActivate {
         HttpStatus.UNAUTHORIZED,
       );
     }
-    let userUid;
+
+    let firebaseToken: DecodedIdToken;
     try {
-      const { uid } = await this.authService.parseAuth(authorization);
-      userUid = uid;
+      firebaseToken = await this.authService.parseAuth(authorization);
     } catch (error) {
       if (error.code === 'auth/id-token-expired') {
         this.logger.warn({
@@ -56,15 +57,41 @@ export class SuperAdminAuthGuard implements CanActivate {
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
+
+    if (!firebaseToken.email_verified || !firebaseToken.firebase.sign_in_second_factor) {
+      this.logger.warn({
+        error: AUTH_GUARD_ERRORS.SUPERADMIN_2FA_REQUIRED,
+        errorMessage: `user does not have 2FA enabled`,
+        status: HttpStatus.UNAUTHORIZED,
+      });
+      throw new HttpException(
+        `SuperAdminAuthGuard - user does not have 2FA enabled`,
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    let user;
     try {
-      const user = await this.userRepository.findOneByOrFail({ firebaseUid: userUid });
+      user = await this.userRepository.findOneByOrFail({ firebaseUid: firebaseToken.uid });
       request['userEntity'] = user;
-      return !!user.isSuperAdmin && user.email.indexOf('@chayn.co') !== -1;
     } catch (error) {
       throw new HttpException(
         `SuperAdminAuthGuard - Error finding user: ${error}`,
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
+    if (!user.isSuperAdmin || !user.email.contains('@chayn.co')) {
+      this.logger.warn({
+        error: AUTH_GUARD_ERRORS.SUPERADMIN_UNAUTHORISED,
+        errorMessage: `unauthorised user without superadmin access or chayn account`,
+        status: HttpStatus.UNAUTHORIZED,
+      });
+      throw new HttpException(
+        `SuperAdminAuthGuard - unauthorised user without superadmin access or chayn account`,
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    return true;
   }
 }
