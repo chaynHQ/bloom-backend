b973c596d5be9dd2a2506ef1cf6a715cfdaf649d
annarhughes <annaraehughes@live.co.uk>
2026-04-20 20:13:01 +0200

    time now

--- src/reporting/reporting.constants.ts
+++ src/reporting/reporting.constants.ts
@@ -1,8 +1,8 @@
 export const CRON_EXPRESSIONS = {
-  daily: '0 9 * * *', // 09:00 every day
-  weekly: '0 9 * * MON', // 09:00 every Monday
-  monthly: '0 9 1 * *', // 09:00 on the 1st of every month
-  quarterly: '0 9 1 1,4,7,10 *', // 09:00 on the 1st of Jan/Apr/Jul/Oct
+  daily: '25 19 * * *', // 19:25 every day
+  weekly: '25 19 * * MON', // 19:25 every Monday
+  monthly: '25 19 1 * *', // 19:25 on the 1st of every month
+  quarterly: '25 19 1 1,4,7,10 *', // 19:25 on the 1st of Jan/Apr/Jul/Oct
 } as const;
 
 // Slack's absolute cap is 50 blocks per message; stop short at 45 so the
