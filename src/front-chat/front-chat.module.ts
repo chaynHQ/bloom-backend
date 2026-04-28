31c45978da8e73e99f672359693c57ce8eed92b0
annarhughes <annaraehughes@live.co.uk>
2026-04-28 15:29:10 +0200

    fix

--- src/front-chat/front-chat.module.ts
+++ src/front-chat/front-chat.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
 import { FrontChatController } from './front-chat.controller';
 import { FrontChatService } from './front-chat.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [FrontChatService],
   controllers: [FrontChatController],
   exports: [FrontChatService],
