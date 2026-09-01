7d8db22744bf179698c6d303a74e421244999a5d
haseeb-iqbal <a.haseeb19@gmail.com>
2026-09-01 19:34:35 +0500

    Removed unused files

--- src/partner-feature/partner-feature.interface.ts
+++
@@ -1,8 +0,0 @@
-import { IFeature } from 'src/feature/feature.interface';
-
-export interface IPartnerFeature {
-  partnerId: string;
-  featureId: string;
-  active: boolean;
-  feature: IFeature;
-}
